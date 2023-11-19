dcdb6defc060625a1bcfffab52c7995d3fe9337b
MarcoGotti <mrc.gotti@gmail.com>
2023-11-19 23:34:26 +0000

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// what  i need
+/* 
+variable
+prompt 
+*/
+
+
+const firstName = prompt ('Type your first-name');
+console.log (firstName);
+
+const lastName = prompt ('Good! Now type your last-name');
+console.log (lastName);
+
+const favourite_color = prompt ('One last info: your favourite color');
+console.log (favourite_color);
