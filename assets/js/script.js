b3b04f4b3ef9fcb4e5bb19e6781e17c6ccd0c956
MarcoGotti <mrc.gotti@gmail.com>
2023-11-20 12:08:35 +0000

    trying differnt methods

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,25 @@ console.log (favourite_color);
 console.log (firstName + lastName + favourite_color + '23');
 
 document.getElementById('before-pwd-text').innerHTML = ('Log-in into your account with this generated password');
-//`Dear customer, with this password you can enter your account again ${firstName + lastName + favourite_color}23`;
 document.getElementById('generated-password').innerHTML = (firstName + lastName + favourite_color + '23');
 document.getElementById('after-pwd-text').innerHTML = ('Once you have logged in you can change it by going on Settings');
 document.getElementById('assistance-pwd-text').innerHTML = ('For any problem contact us on luigi.assistance@boolean.it');
+
+//just trying around: backtick
+console.log (
+    `
+    Dear customer, this is your password 
+    ${firstName + lastName + favourite_color}23
+    `
+    );
+    
+    document.getElementById('generated-password-backtick').innerHTML = 
+    `
+    Dear customer, this is your password: 
+    ${firstName + lastName + favourite_color}23
+    `;
+
+//just trying around: backtick2
+document.getElementById('backtick2-text').innerHTML = 'Dear cusomer, your password again'    
+document.getElementById('generated-password-backtick2').innerHTML = (firstName + lastName + favourite_color + '23');
+    
